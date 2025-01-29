856fed57dc4a3ddefd9a5c641c1b4a7a2cf808fd
Unicornelia <ez.kornelia@gmail.com>
2025-01-29 16:27:37 +0100

    displaying a simple html with node.js

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
 
 	res.setHeader('Content-Type', 'text/html');
+
+	res.write('<!DOCTYPE html>');
+	res.write('<html lang="en">');
+	res.write('<head><title>Node server based Html</title></head>');
+	res.write('<body><h2>Greetings from Node.js server!</h2></body>');
+	res.write('</html>');
+	res.end();
 });
 
 server.listen(3000);
