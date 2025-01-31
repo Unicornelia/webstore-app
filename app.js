8dec78ae838fc14feea59e6669a5fe05660408d9
Unicornelia <ez.kornelia@gmail.com>
2025-01-31 18:35:17 +0100

    fix: using routing in express

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/add-greeting', (req, res, next) => {
+	res.send('<form action="/greeting" method="post"><input type="text" name="title"><button type="submit">Add Greeting</button></form>');
+});
+
+app.post('/greeting', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+
 app.get('/', (req, res, next) => {
-	res.send('<h1>Hello!!</h1>');
+	res.send('<h1>Welcome!</h1>');
 });
 
 app.listen(3000);
