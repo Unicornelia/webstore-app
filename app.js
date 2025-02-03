fa1507f7c341470a928ab3ef909580bd7dc0ffad
Unicornelia <ez.kornelia@gmail.com>
2025-02-03 17:34:40 +0100

    feat: add prettier to project

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin/', adminData.routes);
+app.use("/admin/", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).render('404', {pageTitle: 'Page not found'});
+  res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 app.listen(3000);
