0488b62b559af7f5fbaaf49796061ba550ddec6c
Unicornelia <ez.kornelia@gmail.com>
2025-02-03 17:35:09 +0100

    feat: extract to controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,21 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const { getAddProduct } = require("../controllers/products");
 
-const products = []
+const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-	res.render('add-product', {
-		pageTitle: 'Add Product',
-		path: '/admin/add-product',
-		formsCSS: true,
-		productCSS: true,
-		activeAddProduct: true
-	});
-});
+router.get("/add-product", getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-	products.push({ title: req.body.title });
-	res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
-
 
 exports.routes = router;
 exports.products = products;
