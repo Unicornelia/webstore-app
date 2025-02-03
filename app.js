a374afb7ee87d17b1a5891cb7f94e5d346675c88
Unicornelia <ez.kornelia@gmail.com>
2025-02-03 16:39:34 +0100

    feat: use templating engine ejs instead of html

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +17,7 @@ app.use('/admin/', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+	res.status(404).render('404', {pageTitle: 'Page not found'});
 });
 
 app.listen(3000);
