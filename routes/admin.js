b677486c94f9a95bbc268ded70f51da46ae1f8bf
Unicornelia <ez.kornelia@gmail.com>
2025-02-03 15:36:22 +0100

    fix: capture data locally in a variable

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+const router = express.Router();
+
 const rootDir = require('../util/path');
 
-const router = express.Router();
+const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -13,8 +15,10 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
 	console.log(req.body);
+	products.push({title: req.body.title});
 	res.redirect('/');
 });
 
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
