dba5f6e601218d67b45010b540c2d5cc6777a0c0
Unicornelia <ez.kornelia@gmail.com>
2025-02-03 17:56:11 +0100

    feat: extract 404 to its own controller

--- app.js
+++ app.js
@@ -9,15 +9,13 @@ app.set("views", path.join(__dirname, "views"));
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const { get404Page } = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin/", adminRoutes);
 app.use(shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(get404Page);
 
 app.listen(3000);
