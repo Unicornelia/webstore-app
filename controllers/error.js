3731de97634da75c5244c4903c3083879a43ff70
Unicornelia <ez.kornelia@gmail.com>
2025-02-04 11:33:22 +0100

    fix: 404 error page

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,8 @@
 exports.get404Page = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
+  res.status(404).render("404", {
+    pageTitle: "Page not found",
+    path: "/404",
+    formsCSS: true,
+    productCSS: true,
+  });
 };
