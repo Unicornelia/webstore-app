68091dcea3e2e79dc1c5f70cdb1e2ae5db757d92
Unicornelia <ez.kornelia@gmail.com>
2025-02-06 16:43:47 +0100

    feat: expand product model and add more details to views, set up new routes for later

--- models/product.js
+++ models/product.js
@@ -1,38 +1,41 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
-)
+);
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, 'utf8', (err, data) => {
     if (err) {
-      console.log(`Error in reading from file: ${err}`)
-      cb([])
+      console.log(`Error in reading from file: ${err}`);
+      cb([]);
     } else {
-      cb(JSON.parse(data))
+      cb(JSON.parse(data));
     }
-  })
-}
+  });
+};
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile((products) => {
-      products.push(this)
+      products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(`Error in writing to file: ${err}`)
-      })
-    })
+        console.log(`Error in writing to file: ${err}`);
+      });
+    });
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb)
+    getProductsFromFile(cb);
   }
-}
+};
