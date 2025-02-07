10ac4a1a370b5025d16e7dd585e641bf42dd1452
Unicornelia <ez.kornelia@gmail.com>
2025-02-07 16:32:01 +0100

    feat: extend product model with findById method and update controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,8 +22,10 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProductDetail = (req, res, next) => {
-  const prodId = req.params.productId;
-  console.log(prodId);
+  const { productId } = req.params;
+  Product.findById(productId, (product) => {
+    console.log(product);
+  });
   res.redirect('/');
 };
 
