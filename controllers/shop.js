94ef2782a87db88d867021200aec770b8f4d056a
Unicornelia <ez.kornelia@gmail.com>
2025-02-07 13:42:34 +0100

    style updates and products new route

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
-const adminData = require('../routes/admin')
-const Product = require('../models/Product')
+const adminData = require('../routes/admin');
+const Product = require('../models/Product');
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -7,9 +7,9 @@ exports.getIndex = (req, res, next) => {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
-    })
-  })
-}
+    });
+  });
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -17,20 +17,27 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: 'All Products',
       path: '/products',
-    })
-  })
-}
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     pageTitle: 'Your Cart',
     path: '/cart',
-  })
-}
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Your Orders',
+    path: '/orders',
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     pageTitle: 'Your Checkout',
     path: '/checkout',
-  })
-}
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,19 @@
-const express = require('express')
+const express = require('express');
 
 const {
   getProducts,
   getIndex,
   getCart,
   getCheckout,
-} = require('../controllers/shop')
+  getOrders,
+} = require('../controllers/shop');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', getIndex)
-router.get('/products', getProducts)
-router.get('/cart', getCart)
-router.get('/checkout', getCheckout)
+router.get('/', getIndex);
+router.get('/products', getProducts);
+router.get('/cart', getCart);
+router.get('/orders', getOrders);
+router.get('/checkout', getCheckout);
 
-module.exports = router
+module.exports = router;
