ddf483d0a32b38f2465764224accc6c31f5b0dae
Unicornelia <ez.kornelia@gmail.com>
2025-03-27 12:21:39 +0100

    feat: add validation to some admin routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,17 +4,37 @@ const {
   postAddProduct,
   getEditProduct,
   postEditProduct,
-  deleteProduct, getAddProduct,
+  deleteProduct,
+  getAddProduct,
 } = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
 router.get('/products', isAuth, getProducts);
 router.get('/add-product', isAuth, getAddProduct);
-router.post('/add-product', isAuth, postAddProduct);
+router.post(
+  '/add-product',
+  [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 4, max: 400 }).trim(),
+  ],
+  isAuth,
+  postAddProduct
+);
 router.get('/edit-product/:id', isAuth, getEditProduct);
-router.post('/edit-product', isAuth, postEditProduct);
+router.post(
+  '/edit-product',
+  [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 4, max: 400 }).trim(),
+  ],
+  isAuth,
+  postEditProduct
+);
 router.delete('/delete-product/:id', isAuth, deleteProduct);
 
 module.exports = router;
