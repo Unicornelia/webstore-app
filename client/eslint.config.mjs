21a4b57dd59f19fa1491c2440f22ff1b314c5ae1
Unicornelia <ez.kornelia@gmail.com>
2025-03-27 18:04:41 +0100

    fix: update packages and config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -5,6 +5,7 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.es2025 } },
+  { extends: ['react-app'] },
   {
     plugins: {
       eslintPluginPrettier,
